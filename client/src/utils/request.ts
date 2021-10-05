ce7eb1eb14da31d35eb9458c40035655a04d1330
Balleys1900 <leminhtai325@gmail.com>
2021-10-05 19:56:55 +0700

    Add JS for safari and implement toggle pause Video for mobile

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 // Create an axios instance
 
 const service = axios.create({
-  baseURL: 'https://chatbot-version-11.herokuapp.com/api/v1/node',
+  // baseURL: 'https://chatbot-version-11.herokuapp.com/api/v1/node',
+  baseURL: 'http://localhost:4000/api/v1/node',
   timeout: 5000,
   headers: {
     accept: 'application/json',
