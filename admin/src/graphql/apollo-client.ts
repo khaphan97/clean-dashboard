65305a860fd4314d18171b5b198f0b402d312342
Phan Huy Kha <66725049+khaphan97@users.noreply.github.com>
2021-10-11 14:19:00 +0700

    Update apollo-client.ts

--- admin/src/graphql/apollo-client.ts
+++ admin/src/graphql/apollo-client.ts
@@ -2,8 +2,8 @@ import ApolloClient from 'apollo-boost';
 
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
-  // uri: 'https://chatbot-version-11.herokuapp.com/graphql',
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://chatbot-version-11.herokuapp.com/graphql',
+//   uri: 'http://localhost:4000/graphql',
 });
 
 export default apolloClient;
